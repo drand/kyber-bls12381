64fb6fb1d04c47bfdcdf9dea48115a5b92b215a5
nikkolasg <nikkolasg@gmail.com>
2020-09-17 21:19:50 +0100

    test that shouldn't fail

--- kyber_test.go
+++ kyber_test.go
@@ -359,3 +359,45 @@ func TestIsValidGroup(t *testing.T) {
 	require.True(t, p1.(GroupChecker).IsInCorrectGroup())
 	require.True(t, p2.(GroupChecker).IsInCorrectGroup())
 }
+
+func TestBasicPairing(t *testing.T) {
+	suite := NewBLS12381Suite()
+	NewElement := func() kyber.Scalar {
+		return suite.G1().Scalar()
+	}
+	NewG1 := func() kyber.Point {
+		return suite.G1().Point()
+	}
+	NewG2 := func() kyber.Point {
+		return suite.G2().Point()
+	}
+	Pair := func(a, b kyber.Point) kyber.Point {
+		return suite.Pair(a, b)
+	}
+	// we test a * b = c + d
+	a := NewElement().Pick(random.New())
+	b := NewElement().Pick(random.New())
+	c := NewElement().Pick(random.New())
+	d := NewElement().Sub(NewElement().Mul(a, b), c)
+
+	// check in the clear
+	ab := NewElement().Mul(a, b)
+	cd := NewElement().Add(c, d)
+	require.True(t, ab.Equal(cd))
+
+	// check in the exponent now with the following
+	// e(aG1,bG2) = e(cG1,G2) * e(G1,dG2) <=>
+	// e(G1,G2)^(a*b) = e(G1,G2)^c * e(G1,G2)^d
+	// e(G1,G2)^(a*b) = e(G1,G2)^(c + d)
+	aG := NewG1().Mul(a, nil)
+	bG := NewG2().Mul(b, nil)
+	left := Pair(aG, bG)
+
+	cG := NewG1().Mul(c, nil)
+	right1 := Pair(cG, NewG2())
+	dG := NewG2().Mul(d, nil)
+	right2 := Pair(NewG1(), dG)
+	right := right1.Add(right1, right2)
+
+	require.True(t, left.Equal(right))
+}
