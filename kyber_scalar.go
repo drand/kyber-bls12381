5b969e3b833db54020bd495d7a21659d68481b11
nikkolasg <nikkolasg@gmail.com>
2022-07-19 15:31:54 +0100

    wip

--- kyber_scalar.go
+++ kyber_scalar.go
@@ -22,23 +22,15 @@ func (s *Scalar) Hash(h kyber.HashFactory, msg []byte) kyber.Scalar {
 	canonicalBitLen := s.Int.MarshalSize() * 8
 	actualBitLen := s.Int.V.BitLen()
 	toMask := canonicalBitLen - actualBitLen
-	var fullMask byte = 0xff
-	var mask byte = 0
-	for toMask > 0 {
-		mask = mask<<1 + 1
-		toMask = toMask >> 1
-	}
-	mask = mask & fullMask
-
 	buff := msg
 	for {
 		hash := h.Hash()
 		_, _ = hash.Write(buff)
 		buff = hash.Sum(nil)
 		if s.Int.BO == mod.LittleEndian {
-			buff[0] = buff[0] & mask
+			buff[0] = buff[0] >> toMask
 		} else {
-			buff[len(buff)-1] = buff[len(buff)-1] & mask
+			buff[len(buff)-1] = buff[len(buff)-1] >> toMask
 		}
 		if err := s.UnmarshalBinary(buff); err == nil {
 			return s
